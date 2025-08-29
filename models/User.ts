489c378e46ec2f40f0a71b4a416d96983100106a
Karan <kb466444@gmail.com>
2025-08-29 17:42:54 +0530

    fix: User model

--- models/User.ts
+++ models/User.ts
@@ -5,4 +5,4 @@ const UserSchema = new mongoose.Schema({
     email:{type:String,required:true},
 });
 
-export default mongoose.model("User",UserSchema);
+export default mongoose.models.User || mongoose.model("User", UserSchema);
